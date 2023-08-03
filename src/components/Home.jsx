c8688c4e21adfe131c526fe82edf1c6740d130be
Adnan <adnan.hussain.136660@gmail.com>
2023-08-03 22:50:10 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 const Home = () => {
     return (
         <section
-            className={`bg-[url(./assets/images/star-background.jpg)] bg-no-repeat bg-cover bg-center bg-fixed relative pt-[120px] home h-full sm:min-h-full sm:h-[100vh]`}
+            className={`bg-[url(./assets/images/star-background.jpg)] bg-no-repeat bg-cover bg-center bg-fixed relative pt-[120px] home h-full min-h-full sm:min-h-full sm:h-[100vh]`}
         >
             <div
                 className="container m-auto flex flex-col-reverse sm:flex-row justify-between items-center relative h-full"
